55067eceb054df830eb4ed2bc6f395876c2da806
Jingchun Wang <jingchun@cadence.com>
2024-01-17 11:06:43 -0800

    opt for even N-ROSY output

--- frame_field/ffio.cc
+++ frame_field/ffio.cc
@@ -106,7 +106,7 @@ int save_vertex_vector(const TriMesh &mesh, const char *var_vec, const char *fil
         filename);
 }
 
-int save_face_n_rosy(const TriMesh &mesh, const char *var_vec, const int n_rosy, const char *filename, const double scale, const double offset)
+static int save_face_n_rosy_odd(const TriMesh &mesh, const char *var_vec, const int n_rosy, const char *filename, const double scale, const double offset)
 {
     if (!hasProperty<Fh, Vec3>(mesh, var_vec)) return 1;
     auto f_v = getProperty<Fh, Vec3>(mesh, var_vec);
@@ -140,7 +140,49 @@ int save_face_n_rosy(const TriMesh &mesh, const char *var_vec, const int n_rosy,
         filename);
 }
 
-int save_vertex_n_rosy(const TriMesh &mesh, const char *var_vec, const int n_rosy, const char *filename, const double scale, const double offset)
+static int save_face_n_rosy_even(const TriMesh &mesh, const char *var_vec, const int n_rosy, const char *filename, const double scale, const double offset)
+{
+    if (!hasProperty<Fh, Vec3>(mesh, var_vec)) return 1;
+    auto f_v = getProperty<Fh, Vec3>(mesh, var_vec);
+
+    const double dt = pi<double>()*2. / n_rosy;
+
+    std::vector<Vec3> ps {};
+    std::vector<Int2> es {};
+    int np {};
+
+    for (int r = 0; r < n_rosy / 2; ++r)
+    {
+        const double t = dt * r;
+
+        for (const auto face : mesh.faces())
+        {
+            const auto p = mesh.calc_face_centroid(face);
+            const auto n = mesh.calc_normal(face) * offset;
+            const auto d = rotate(f_v[face], n, t) * scale;
+            ps.push_back(p + n - d);
+            ps.push_back(p + n + d);
+            es.push_back({ np+1, np+2 });
+            np += 2;
+        }
+    }
+
+    return save_obj(
+        (const double*)ps.data(), (const int)ps.size(),
+        nullptr,                  0,
+        (const int*)es.data(),    (const int)es.size(),
+        filename);
+}
+
+int save_face_n_rosy(const TriMesh &mesh, const char *var_vec, const int n_rosy, const char *filename, const double scale, const double offset)
+{
+    if (n_rosy % 2) // odd
+        return save_face_n_rosy_odd (mesh, var_vec, n_rosy, filename, scale, offset);
+    else // even
+        return save_face_n_rosy_even(mesh, var_vec, n_rosy, filename, scale, offset);
+}
+
+static int save_vertex_n_rosy_odd(const TriMesh &mesh, const char *var_vec, const int n_rosy, const char *filename, const double scale, const double offset)
 {
     if (!hasProperty<Vh, Vec3>(mesh, var_vec)) return 1;
     auto v_v = getProperty<Vh, Vec3>(mesh, var_vec);
@@ -172,6 +214,48 @@ int save_vertex_n_rosy(const TriMesh &mesh, const char *var_vec, const int n_ros
         nullptr,                  0,
         (const int*)es.data(),    (const int)es.size(),
         filename);
+}
+
+static int save_vertex_n_rosy_even(const TriMesh &mesh, const char *var_vec, const int n_rosy, const char *filename, const double scale, const double offset)
+{
+    if (!hasProperty<Vh, Vec3>(mesh, var_vec)) return 1;
+    auto v_v = getProperty<Vh, Vec3>(mesh, var_vec);
+
+    const double dt = pi<double>()*2. / n_rosy;
+
+    std::vector<Vec3> ps {};
+    std::vector<Int2> es {};
+    int np {};
+
+    for (int r = 0; r < n_rosy / 2; ++r)
+    {
+        const double t = dt * r;
+
+        for (const auto vert : mesh.vertices())
+        {
+            const auto p = mesh.point(vert);
+            const auto n = mesh.calc_normal(vert) * offset;
+            const auto d = rotate(v_v[vert], n, t) * scale;
+            ps.push_back(p + n - d);
+            ps.push_back(p + n + d);
+            es.push_back({ np+1, np+2 });
+            np += 2;
+        }
+    }
+
+    return save_obj(
+        (const double*)ps.data(), (const int)ps.size(),
+        nullptr,                  0,
+        (const int*)es.data(),    (const int)es.size(),
+        filename);
+}
+
+int save_vertex_n_rosy(const TriMesh &mesh, const char *var_vec, const int n_rosy, const char *filename, const double scale, const double offset)
+{
+    if (n_rosy % 2) // odd
+        return save_vertex_n_rosy_odd (mesh, var_vec, n_rosy, filename, scale, offset);
+    else // even
+        return save_vertex_n_rosy_even(mesh, var_vec, n_rosy, filename, scale, offset);
 }
 
 int save_selected_face_centroids(const TriMesh &mesh, const char *filename, const double offset)
